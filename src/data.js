c5df8a13c8c24d604dcbbf759f9f1b7f0bd181fd
em-111 <endritmehaj33@gmail.com>
2024-03-28 00:20:14 +0100

    Personal Portfolio

--- src/data.js
+++ src/data.js
@@ -1,17 +1,81 @@
+import img0 from "./assets/websites/img-0.avif";
+import img1 from "./assets/websites/img-1.avif";
+import img2 from "./assets/websites/img-2.avif";
+import img3 from "./assets/websites/img-3.avif";
+import img4 from "./assets/websites/img-4.avif";
+import img5 from "./assets/websites/img-5.avif";
+import img6 from "./assets/websites/img-6.avif";
+import img7 from "./assets/websites/img-7.avif";
+import img8 from "./assets/websites/img-8.avif";
+import img9 from "./assets/websites/img-9.avif";
+import img10 from "./assets/websites/img-10.avif";
+import img11 from "./assets/websites/img-11.avif";
+import img12 from "./assets/websites/img-12.avif";
+
 export const data = [
     {
-        img: "assets/websites/img-0.avif",
+        img: img0,
         url: "https://11testimonials.netlify.app",
         github: "https://github.com/em-111/Testimonials",
     },
     {
-        img: "/src/assets/websites/img-1.avif",
-        url: "https://11testimonials.netlify.app",
-        github: "https://github.com/em-111/Testimonials",
+        img: img1,
+        url: "https://11gallery.netlify.app",
+        github: "https://github.com/em-111/My-Gallery",
     },
     {
-        img: "../assets/websites/img-2.avif",
-        url: "https://11testimonials.netlify.app",
-        github: "https://github.com/em-111/Testimonials",
+        img: img2,
+        url: "https://11homepage.netlify.app",
+        github: "https://github.com/em-111/Homepage",
+    },
+    {
+        img: img3,
+        url: "https://11checkoutpage.netlify.app",
+        github: "https://github.com/em-111/Checkout-Page",
+    },
+    {
+        img: img4,
+        url: "https://11sunnyside.netlify.app",
+        github: "https://github.com/em-111/Sunnyside-Agency",
+    },
+    {
+        img: img5,
+        url: "https://11huddle.netlify.app",
+        github: "https://github.com/em-111/Huddle",
+    },
+    {
+        img: img6,
+        url: "https://11clipboard.netlify.app",
+        github: "https://github.com/em-111/Clipboard",
+    },
+    {
+        img: img7,
+        url: "https://11hangman.netlify.app",
+        github: "https://github.com/em-111/Hangman",
+    },
+    {
+        img: img8,
+        url: "https://11country-quiz.netlify.app",
+        github: "https://github.com/em-111/Country-Quiz",
+    },
+    {
+        img: img9,
+        url: "https://11todo-list.netlify.app",
+        github: "https://github.com/em-111/Todo-List",
+    },
+    {
+        img: img10,
+        url: "https://11country-quiz2.netlify.app",
+        github: "https://github.com/em-111/Country-Quiz-2",
+    },
+    {
+        img: img11,
+        url: "https://11worldcountries.netlify.app",
+        github: "https://github.com/em-111/World-Countries",
+    },
+    {
+        img: img12,
+        url: "https://11components.netlify.app",
+        github: "https://github.com/em-111/Components",
     },
 ];
